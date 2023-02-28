1ff5973e58e492512fd954ea0db6e824bb859be6
Vinicius Bertolo <vini.alex.bertolo@gmail.com>
2023-02-28 19:55:47 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,222 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const res = require("express/lib/response");
+const { redirect } = require("express/lib/response");
+const saltRounds = 10;
+
+{
+  /*Conexão Com o banco de dados */
+}
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "vamb1808",
+  database: "banco",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const profissao = req.body.profissao;
+  const nome = req.body.nome;
+  const phone = req.body.phone;
+  const cep = req.body.cep;
+  const logradouro = req.body.logradouro;
+  const numero = req.body.numero;
+  const bairro = req.body.bairro;
+  const cidade = req.body.cidade;
+  const estado = req.body.estado;
+  const data_nascimento = req.body.data_nascimento;
+
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length == 0) {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        db.query(
+          "INSERT INTO usuarios (email, password, profissao, nome, phone, cep, logradouro, numero, bairro, cidade, estado, data_nascimento) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+          [
+            email,
+            hash,
+            profissao,
+            nome,
+            phone,
+            cep,
+            logradouro,
+            numero,
+            bairro,
+            cidade,
+            estado,
+            data_nascimento,
+          ],
+          (err, response) => {
+            if (err) {
+              res.send(err);
+            }
+
+            res.send({ msg: "Usuário cadastrado com sucesso" });
+          }
+        );
+      });
+    } else {
+      res.send({ msg: "Email já cadastrado!" });
+    }
+  });
+});
+
+{
+  /*Verificação de login*/
+}
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length > 0) {
+      bcrypt.compare(password, result[0].password, (error, response) => {
+        if (error) {
+          res.send(error);
+        }
+        if (response === true) {
+          res.send(response);
+          console.log(response);
+        } else {
+          res.send({ msg: "Email ou senha incorreta!" });
+        }
+      });
+    } else {
+      res.send({ msg: "Usuário não registrado!" });
+    }
+  });
+});
+
+app.get("/cursos", (req, res) => {
+  let sql = "SELECT *  FROM curso";
+  db.query(sql, (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+});
+
+app.get("/fases/:id", (req, res) => {
+  const id = req.params.id;
+  db.query(
+    "SELECT *  FROM fases where ID_CURSO_ATUAL = ?",
+    [id],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    }
+  );
+});
+
+app.get("/videos/:id/:nroPilula", (req, res) => {
+  const id = req.params.id;
+  const nroPilula = req.params.nroPilula;
+  db.query(
+    "SELECT * FROM fases where ID_CURSO_ATUAL = ? and NRO = ?",
+    [id, nroPilula],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    }
+  );
+});
+app.get("/perguntas/:id/:nroPilula", (req, res) => {
+  const id = req.params.id;
+  const nroPilula = req.params.nroPilula;
+  db.query(
+    "SELECT * FROM alternativas where ID_cursoAtual = ? and NRO_FASE = ?",
+    [id, nroPilula],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    }
+  );
+});
+
+app.get("/questao/:id/:nroPilula", (req, res) => {
+  const id = req.params.id;
+  const nroPilula = req.params.nroPilula;
+  db.query(
+    "SELECT * FROM fases where ID_CURSO_ATUAL = ? and NRO = ?",
+    [id, nroPilula],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    }
+  );
+});
+
+app.get("/caminho-concluido/:email/:idCurso", (req, res) => {
+  const email = req.params.email;
+  const idCurso = req.params.idCurso;
+  db.query(
+    "SELECT * FROM conclui_etapa where emailUsuario = ? and ID_usuario = ?",
+    [email, idCurso],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    }
+  );
+});
+
+app.post("/inserir-primeiro-caminho/:email/:idCurso/:nroFase", (req, res) => {
+  const email = req.params.email;
+  const idCurso = req.params.idCurso;
+  const nroFase = req.params.nroFase;
+
+  db.query(
+    "SELECT * FROM conclui_etapa WHERE emailUsuario = ? and ID_usuario = ? and NRO_FASE_ATUAL =?",
+    [email, idCurso, nroFase],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      if (result.length == 0) {
+        db.query(
+          "INSERT INTO conclui_etapa (emailUsuario, ID_usuario, NRO_FASE_ATUAL) values (?,?,?)",
+          [email, idCurso, nroFase],
+          (err, result) => {
+            if (err) console.log(err);
+            else res.send(result);
+          }
+        );
+      } else {
+        res.send({ msg: "Já cadastrada fase 1 ou superior" });
+      }
+    }
+  );
+});
+
+app.put("/pergunta-acertada/:email/:idCurso/:nroFase/:nroFaseAnterior", (req, res) => {
+  const email = req.params.email;
+  const idCurso = req.params.idCurso;
+  const nroFase = req.params.nroFase;
+  const nroFaseAnterior =req.params.nroFaseAnterior;
+  db.query(
+    "UPDATE conclui_etapa SET NRO_FASE_ATUAL = ?,emailUsuario = ?, ID_usuario = ? WHERE emailUsuario = ? and ID_usuario = ? and NRO_FASE_ATUAL = ?",
+    [nroFase, email, idCurso,email,idCurso,nroFaseAnterior],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send(result);
+    }
+  );
+});
+
+
+app.listen(3001, () => {
+  console.log("rodando na porta 3001");
+});
